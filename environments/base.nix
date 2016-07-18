fa49e7722fe9b3c0dba59f10bcc7e64dc41d2aef
Tristan Hume <tris.hume@gmail.com>
2016-07-18 15:45:17 -0400

    Basic compile/run script support.

--- environments/base.nix
+++ environments/base.nix
@@ -1,10 +1,34 @@
-{buildInputs}:
+{buildInputs, compileScript ? null, runScript ? null}:
 let
 extraInps = buildInputs;
+scriptVars = ''
+  local inFile="$1"
+  local fileName="$(basename $inFile)"
+  local extension="''${fileName##*.}"
+  local binFile="''${fileName%.*}_$extension"
+'';
+compileFn = ''
+  compile() {
+    ${scriptVars}
+    ${compileScript}
+  }
+'';
+runFn = ''
+  run() {
+    ${scriptVars}
+    ${runScript}
+  }
+'';
 in
 with import <nixpkgs> {};
 stdenv.mkDerivation rec {
   name = "benchmark-stadium";
   env = buildEnv { name = name; paths = buildInputs; };
   buildInputs = extraInps;
+  COMPILE_SCRIPT = compileScript;
+  RUN_SCRIPT = runScript;
+  shellHook = ''
+    ${if compileScript != null then compileFn else ""}
+    ${if runScript != null then runFn else ""}
+  '';
 }
--- environments/go.nix
+++ environments/go.nix
@@ -1,4 +1,10 @@
 with import <nixpkgs> {};
 import ./base.nix {
   buildInputs = [go];
+  compileScript = ''
+    go build -o $binFile $inFile
+  '';
+  runScript = ''
+    ./$binFile
+  '';
 }
